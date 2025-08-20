d013c7fca8f6703e66776201628cbf0becc8bccd
Charith Kaduruwewa <charithkaduru@gmail.com>
2025-08-20 12:26:40 +0530

    Add remaining backend files

---
+++ pahanaedu/src/main/java/com/csk/pahanaedu/PahanaeduApplication.java
@@ -0,0 +1,13 @@
+package com.csk.pahanaedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PahanaeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PahanaeduApplication.class, args);
+	}
+
+}
---
+++ pahanaedu/src/test/java/com/csk/pahanaedu/PahanaeduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csk.pahanaedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PahanaeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
